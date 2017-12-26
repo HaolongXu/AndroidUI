463fab65b288e2608c01c831acc1954f02e7752b
haolun <haolun@iastate.edu>
2017-12-26 17:31:46 -0500

    ProgressBar

--- app/src/main/java/activitytest/example/com/uiwidgettest/MainActivity.java
+++ app/src/main/java/activitytest/example/com/uiwidgettest/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText editText;
 
     private ImageView imageView;
+
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button button = (Button) findViewById(R.id.button);
         editText = (EditText) findViewById(R.id.edit_text);
         imageView = (ImageView) findViewById(R.id.image_view);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
         button.setOnClickListener(this);
     }
 
@@ -29,11 +33,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.button:
+                int progress = progressBar.getProgress();
+                progress = progress + 10;
+                progressBar.setProgress(progress);
+                break;
+
+                //if(progressBar.getVisibility() == View.GONE) {
+                  //  progressBar.setVisibility(View.VISIBLE);
+                //} else{
+                  //  progressBar.setVisibility(View.GONE);
+                //}
+                //break;
+
                 //String inputText = editText.getText().toString();
                 //Toast.makeText(MainActivity.this, inputText,
                 //       Toast.LENGTH_SHORT).show();
-                imageView.setImageResource(R.drawable.f94106dda981878c0377637ab3c2c3d7);
-                break;
+
+                //imageView.setImageResource(R.drawable.f94106dda981878c0377637ab3c2c3d7);
+                //break;
             default:
                 break;
         }
